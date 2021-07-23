23f4c0a79998da76800603bc9116178d6dc770be
YyJqW <1135145127@qq.com>
2021-07-23 18:03:05 +0800

    修改抬头

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+    <title>@yield('title', 'Laravel') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
