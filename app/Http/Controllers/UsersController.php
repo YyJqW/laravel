ffb7b41aabf345b240cf177822022de306bbe7d9
YyJqW <1135145127@qq.com>
2021-07-26 09:49:30 +0800

    注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,6 +22,12 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+        $user = User::create([
+            'name' => $request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password)
+        ]);
+        session()->flash('success','欢迎，注册成功');
+        return redirect()->route('users.show',[$user]);
     }
 }
