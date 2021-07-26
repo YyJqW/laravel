1993093588d064567ced5790cb69712bca9a9c1b
YyJqW <1135145127@qq.com>
2021-07-26 18:16:01 +0800

    访问策略

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,18 +2,13 @@
 
 namespace App\Policies;
 
-use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\User;
 
 class UserPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Create a new policy instance.
-     *
-     * @return void
-     */
     public function update(User $currentUser, User $user)
     {
         return $currentUser->id === $user->id;
