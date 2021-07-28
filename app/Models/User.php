17988ec50ec2c6d443e65062676951bbbe542395
YyJqW <1135145127@qq.com>
2021-07-28 15:50:09 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,7 +59,9 @@ class User extends Authenticatable
     }
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at','desc');
+        $user_ids=$this->following->pluck('id')->toArray();
+        array_push($user_ids,$this->id);
+        return Status::whereIn('user_id',$user_ids)->with('user')->orderBy('created_at','desc');
     }
     public function fans()
     {
