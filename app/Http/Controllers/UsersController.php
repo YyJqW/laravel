ebcc6096dc15a8fd4b213e72d63ed7cb9c02a915
YyJqW <1135145127@qq.com>
2021-07-28 10:10:17 +0800

    配置发送邮箱

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -93,12 +93,10 @@ class UsersController extends Controller
     {
         $view='email.confirm';
         $data=compact('user');
-        $from='summer@example.com';
-        $name='summer';
         $to=$user->email;
         $subject='感谢注册，请确认你的邮箱';
-        Mail::send($view,$data,function ($message) use ($from,$name,$to,$subject){
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view,$data,function ($message) use ($to,$subject){
+            $message->to($to)->subject($subject);
         });
     }
 }
